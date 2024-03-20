dc9c2c57521104588c8b144b66ad38f6137aca16
AgustioMaitimu <agustiomaitimu8@gmail.com>
2024-03-21 02:20:50 +0900

    feat: Initial commit with React, Router, and TailwindCSS boilerplate

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './pages/App.jsx';
+import Appointment from './pages/Appointment.jsx';
+import Collections from './pages/Collections.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/semar-nusantara/',
+    element: <App />,
+  },
+  {
+    path: '/semar-nusantara/collections/',
+    element: <Collections />,
+  },
+  {
+    path: '/semar-nusantara/appointment',
+    element: <Appointment />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
